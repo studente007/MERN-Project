cdbeb0b958b757feb10220bf14a8bebdea715c18
Sheng <stshinglol@gmail.com>
2021-12-25 17:00:08 +0100

    added new UIElement folder and avatar styling

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -4,12 +4,12 @@ import UsersList from "../components/UserList/UsersList";
 
 const Users = () => {
   const USERS = [
-    // {
-    //   // id: 'u1',
-    //   // name: 'Keaanu Rave',
-    //   // image: "https://www.manners.nl/wp-content/uploads/2020/02/neo-look-behind-the-scenes-the-matrix-4-keanu-reeves.jpg",
-    //   // teams: 3,
-    // },
+    {
+      id: 'u1',
+      name: 'Keaanu Rave',
+      image: "https://www.manners.nl/wp-content/uploads/2020/02/neo-look-behind-the-scenes-the-matrix-4-keanu-reeves.jpg",
+      teams: 3,
+    },
   ]; //all capital because dummy data
 
   return <UsersList items={USERS} />;
